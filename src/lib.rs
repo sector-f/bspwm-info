43c3560517df930c1802bdaabebbd63da44556e6
Adam <sector-f@users.noreply.github.com>
2017-04-18 17:20:23 -0400

    Added module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,86 @@
-use std::io::{BufRead, BufReader};
+//! Library for getting information on monitors and workspaces in `bspwm`.
+//! Tested with `bspwm 0.9.1`.
+//!
+//! # Examples
+//!
+//! List the names of the current monitors and the desktops that are on them:
+//!
+//! ````
+//! extern crate bspwm_info;
+//! use bspwm_info::*;
+//!
+//! fn main() {
+//!     let current_info = status().next().unwrap();
+//!     for monitor in current_info.monitors {
+//!         println!("{}:", monitor.name);
+//!         for desktop in monitor.desktops {
+//!             println!("\t{}", desktop.name);
+//!         }
+//!     }
+//! }
+//! ````
+//!
+//! # To-Do
+//!
+//! * Use a different command (probably `bspc wm -d`) to obtain more information than
+//!     `bspc subscribe report` provides.
+//! * Communicate with `bspwm`'s socket directly rather than wrap the `bspc` command.
+//!     Something like `fn status(path: Option<&Path>)`, where `Some(path)` specifies
+//!     the socket location and `None` uses the default location as specified in
+//!     `bspwm`'s man page.
+
+use std::io::{self, BufRead, BufReader};
 use std::process::{Command, Stdio, ChildStdout};
 
-pub fn status() -> Info {
-    Info::new()
+/// Creates a new `WmInfo`
+pub fn status() -> WmInfo {
+    WmInfo::new()
 }
 
-pub struct Info {
-    child_stdout: BufReader<ChildStdout>
+/// An iterator over `WmRoot`s that is created with the `status()` function
+///
+/// Internally, it holds a `BufReader` that collects output from `bspc subscribe report`.
+/// Each call to `next()` blocks until `bspc` prints a new line. That line
+/// is then parsed into a `WmRoot`
+pub struct WmInfo {
+    buffer: String,
+    child_stdout: BufReader<ChildStdout>,
 }
 
-impl Info {
+impl WmInfo {
     fn new() -> Self {
         let output = Command::new("bspc").args(&["subscribe", "report"]).stdout(Stdio::piped()).spawn().expect("Failed to run bspc. Is bspwm installed?");
         let stdout = output.stdout.expect("Failed to get bspc's stdout");
 
-        Info {
+        WmInfo {
+            buffer: String::new(),
             child_stdout: BufReader::new(stdout),
         }
     }
 }
 
-impl Iterator for Info {
-    type Item = Wm;
+impl Iterator for WmInfo {
+    type Item = io::Result<WmRoot>;
 
-    fn next(&mut self) -> Option<Wm> {
-        let mut buffer = String::new();
-        if self.child_stdout.read_line(&mut buffer).unwrap() > 0 {
-            Some(parse_line(&buffer))
-        } else {
-            None
+    fn next(&mut self) -> Option<io::Result<WmRoot>> {
+        self.buffer.clear();
+
+        match self.child_stdout.read_line(&mut self.buffer) {
+            Ok(i) => {
+                if i > 0 {
+                    Some(Ok(parse_line(&self.buffer)))
+                } else {
+                    None
+                }
+            },
+            Err(e) => {
+                Some(Err(e))
+            },
         }
     }
 }
 
-fn parse_line(line: &str) -> Wm {
+fn parse_line(line: &str) -> WmRoot {
     let mut monitors: Vec<Monitor> = Vec::new();
 
     for section in line[1..].split(":") {
@@ -96,11 +143,12 @@ fn parse_line(line: &str) -> Wm {
         }
     }
 
-    Wm { monitors: monitors }
+    WmRoot { monitors: monitors }
 }
 
+/// A list of all the monitors that `bspwm` is aware of
 #[derive(Debug)]
-pub struct Wm {
+pub struct WmRoot {
     pub monitors: Vec<Monitor>
 }
 
@@ -109,6 +157,9 @@ pub struct Monitor {
     pub name: String,
     pub desktops: Vec<Desktop>,
     pub focused: bool,
+    /// The Layout of the currently-focused monitor
+    /// This is only an `Option` because it made parsing `bspc` easier;
+    /// it should never be `None`. To-Do: Make it not an `Option`
     pub layout: Option<Layout>,
 }
 
@@ -125,4 +176,3 @@ pub enum Layout {
     Tiling,
     Monocle,
 }
-
